db14bd735d7d58f6c64c377c9bc886c488e7d12c
sahilpate <sahpatel21@gmail.com>
2023-04-12 00:44:32 -0400

    Un-harcode the AWS account ID
    The stack will be created on behalf of the AWS making the
    cdk synth/deploy calls.

--- minecraft-server/app.py
+++ minecraft-server/app.py
@@ -5,8 +5,14 @@ import aws_cdk as cdk
 
 from minecraft_server.minecraft_server_stack import MinecraftServerStack
 
-
 app = cdk.App()
-MinecraftServerStack(app, "MinecraftServerStack", env=cdk.Environment(account='352387159701', region='us-east-1'))
+MinecraftServerStack(
+    app,
+    "MinecraftServerStack",
+    env=cdk.Environment(
+        account=os.environ['CDK_DEFAULT_ACCOUNT'],
+        region=os.environ['CDK_DEFAULT_REGION'],
+    ),
+)
 
 app.synth()
